8ee30290874453bc60faf76e3cdf7b3afc110efd
Azamat M <aaamoldashev@gmail.com>
2022-06-16 17:30:51 +0500

    completes button hundling task

--- main.js
+++ main.js
@@ -2,30 +2,39 @@ const createTemplateName = (userName, template) =>
   (template.innerHTML = `<strong>${userName.first} ${userName.last}</strong>`);
 
 async function setPerson() {
-  for (i = 0; i < 3; i++) {
     await fetch("https://randomuser.me/api/")
-    .then((res) => res.json())
-    .then((person) => {
-      const name = person.results[0].name;
-      const div = document.createElement("div");
-      div.classList.add("person");
-      const fragment = document.createDocumentFragment();
-      const main = document.querySelector("main");
-      div.innerHTML = createTemplateName(name, div);
-      fragment.append(div.cloneNode(true));
-      main.appendChild(fragment);
-    });
-  }
-}
+      .then((res) => res.json())
+      .then((person) => {
+        const name = person.results[0].name;
+        const div = document.createElement("div");
+        div.classList.add("person");
+        const fragment = document.createDocumentFragment();
+        const main = document.querySelector("main");
+        div.innerHTML = createTemplateName(name, div);
+        fragment.append(div.cloneNode(true));
+        main.appendChild(fragment);
+      });
+};
 
-document.addEventListener("DOMContentLoaded", setPerson);
+const fillListHandler = () => {
+  for (let i = 0; i < 3; i++) {
+    setPerson();
+  }
+ }
+
+document.addEventListener("DOMContentLoaded", fillListHandler);
 
 // task 2 add user to the existing list
 
 // option 1.
 
 // find button
+const button = document.querySelector("#add-user");
 // add listener
+const addUserHandler = () => {
+  setPerson();
+};
+button.addEventListener("click", addUserHandler);
 // Try reuse existing function
 
 // if not work out:
