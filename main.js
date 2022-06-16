ffd0eb73997b6256cfb93aa8b60d0bfecfdbcb1e
Azamat M <aaamoldashev@gmail.com>
2022-06-16 14:34:59 +0500

    rolls out users

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
-const createTemplateName = (userName, template) => template.innerHTML = `<strong>${userName.first} ${userName.last}</strong>`;
+const createTemplateName = (userName, template) =>
+  (template.innerHTML = `<strong>${userName.first} ${userName.last}</strong>`);
 
-function getPersonName(name) {
-  const fragment = document.createDocumentFragment();
-  const div = document.createElement('div');
-  div.classList.add('person');
-  fragment.append(createTemplateName(name, div));
-  return fragment;
-}
-
-function setPersonName () {
-  const main = document.querySelector('main');
-  for (let i = 0; i <= 3; i++) {
-    main.appendChild(getPersonName())
+async function setPerson() {
+  for (i = 0; i < 3; i++) {
+    await fetch("https://randomuser.me/api/")
+    .then((res) => res.json())
+    .then((person) => {
+      const name = person.results[0].name;
+      const div = document.createElement("div");
+      div.classList.add("person");
+      const fragment = document.createDocumentFragment();
+      const main = document.querySelector("main");
+      div.innerHTML = createTemplateName(name, div);
+      fragment.append(div.cloneNode(true));
+      main.appendChild(fragment);
+    });
   }
 }
 
-async function fetchData() {
-  await fetch("https://randomuser.me/api/")
-      .then((res) => res.json())
-      .then(person => console.log(person.results[0].name))
-      .then((person) => setPersonName(getPersonName(person.results[0].name)));
-  }
-
-document.addEventListener("DOMContentLoaded", fetchData);
+document.addEventListener("DOMContentLoaded", setPerson);
